c46d59a245b9fc4753a08f0e1676f58aea6c7a1b
Prajeet <prajeet911@gmail.com>
2023-09-12 11:17:49 +0545

    Initial commit

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,10 @@
-import AboutComponent from '../components/about-component'
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
 
 export default function About() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <AboutComponent />
-      </main>
+    <div>
+      <h1>About Page</h1>
+      <Link href="/">Homepage</Link>
     </div>
   )
 }
